4420d7ee22a71589b2ece468a9c0fdeb312923a4
Niyam MSB <niyammansinghbasnet@gmail.com>
2024-12-12 21:08:45 +0545

    Update assign.c

--- assign.c
+++ assign.c
@@ -1,34 +1,21 @@
 #include <stdio.h>
 #include <time.h>
-#include <stdlib.h>
 
-void stopwatch() {
+int main() {
     time_t start, end;
     char input;
     double elapsed;
-    printf("Stopwatch started! Press 's' to stop.\n");
+
+    printf("Press Enter to start the stopwatch...");
+    getchar();
     time(&start);
-    do {
-        input = getchar();
-    } while (input != 's');
-    time(&end); 
+
+    printf("Press Enter to stop the stopwatch...");
+    getchar();
+    time(&end);
+
     elapsed = difftime(end, start);
     printf("Elapsed time: %.2f seconds\n", elapsed);
-}
-int main() {
-    char choice;
-    printf("Welcome to the Stopwatch Program!\n");
-    printf("Press 's' to start the stopwatch, or 'q' to quit: ");
-    while (1) {
-        choice = getchar();
-        if (choice == 's') {
-            stopwatch();
-            printf("Press 's' to start again, or 'q' to quit: ");
-        } else if (choice == 'q') {
-            printf("Exiting the program. Goodbye!\n");
-            break;
-        }
-    }
-    printf("This program was made by Niyam");
+
     return 0;
 }
